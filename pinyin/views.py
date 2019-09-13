45c9a01620e6c8eb66449d237e30cc56a6e35bbf
Jerod Sun <Jerod.sun@live.com>
2019-09-13 17:27:30 -0400

    correct views

--- pinyin/views.py
+++ pinyin/views.py
@@ -1,27 +1,28 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-from .models import PinyinCodes
+from .models import PinyinCodes, UserInput
 from .serializers import UserInputSerializer
 from .logic.matcher import match_pinyin
-
 
 # Create your views here.
 
 class UserInputView(viewsets.ModelViewSet):
     serializer_class = UserInputSerializer
-    queryset = PinyinCodes.objects.all()
+    queryset = UserInput.objects.all()
 
-    http_method_names = ['post']
+    http_method_names = ['get', 'post']
 
     def create(self, request):
-        # You need to CREATE into a seperate table, but use the QUERYSET as a reference.
+        # CREATE into a seperate table, but use the QUERYSET as a reference.
         
         serializer = UserInputSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save(ip_address=self.get_client_ip(request))
             d = serializer.validated_data
-            matched_string = match_pinyin(d, PinyinCodes)
-            return Response({'message':'success', 'result':matched_string})
+            matched_string = match_pinyin(d['input_string'], PinyinCodes)
+
+            return Response({'message':'success', 'result': matched_string })
 
         # catch errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
